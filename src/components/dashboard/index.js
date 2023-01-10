3d5bd6a20efb9131566b21d6f1359b4157a3ed39
winadm1n <winadm1n@protonmail.com>
2023-01-10 12:24:37 +0530

    final stage

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,78 +1,79 @@
-import React,{ useState, useEffect } from 'react';
-import Graph from '../graph';
+import React, { useState, useEffect } from "react";
+import Graph from "../graph";
 
 import Header from "../header";
 
 const Dashboard = () => {
-  const [coords, setCoords] = useState({});
   const [coordsAr, setCoordsAr] = useState([]);
- const [titles, setTitles] = useState([]);
-    const getDataJson = async () => {
-      console.log("in get data")
-          const CordinatesUrl ="http://localhost:3001/dashboard";
-          const response = await fetch(CordinatesUrl, {
-            method: "GET",
-            headers: {"Content-Type": "application/json"},
-            })
-            .then((response) => {
-              if (response.ok) {
-                return response.json();
-              } else {
-                throw new Error(response.statusText);
-              }
-            })
-            .then((data) => {
-              setCoordsAr(data);
-              console.log(coordsAr)
-            })
-            .catch((err) => console.log(err));
-    }
+  const [titles, setTitles] = useState([]);
 
-    const getData = async () => {
-      console.log("in get data")
-          const CordinatesUrl ="http://localhost:3001/dashboard";
-          const response = await fetch(CordinatesUrl, {
-            method: "GET",
-            headers: {"Content-Type": "application/json"},
-            })
-            .then((response) => {
-              if (response.ok) {
-                return response.arrayBuffer();
-              } else {
-                throw new Error(response.statusText);
-              }
-            })
-            .then((data) => {
-              console.log("data",data)
-              let title=[];
-              data.map(each =>{
-                    title.push(each.shift())
-                })
-                console.log("title: ",title)
-              setTitles(title);
-              setCoordsAr(data);
-            })
-            .catch((err) => console.log(err));
-    }
-    
+  //uncomment for using with json data after converting from backend
 
-    useEffect(() => {
-      getData();
-      // fetch("http://localhost:3001/")
-      // .then((response) => response.json())
-      // .then((data) => setCoords(data));
-    },[])
+  //const [coords, setCoords] = useState({});
 
-    return (
-        
-        <div id="whole">
-            <Header />
-            <br /><br />
-            <button onClick={getData}>refresh</button> <br />
-            <Graph data={coords} dataAr={coordsAr} titles={titles}/>
+  // const getDataJson = async () => {
+  //   console.log("in get data")
+  //       const CordinatesUrl ="http://localhost:3001/dashboard";
+  //       const response = await fetch(CordinatesUrl, {
+  //         method: "GET",
+  //         headers: {"Content-Type": "application/json"},
+  //         })
+  //         .then((response) => {
+  //           if (response.ok) {
+  //             return response.json();
+  //           } else {
+  //             throw new Error(response.statusText);
+  //           }
+  //         })
+  //         .then((data) => {
+  //           setCoordsAr(data);
+  //           console.log(coordsAr)
+  //         })
+  //         .catch((err) => console.log(err));
+  // }
 
-            
-      </div>
-    );
-}
+  const getData = async () => {
+    console.log("in get data");
+    const CordinatesUrl = "http://localhost:3001/dashboard";
+    const response = await fetch(CordinatesUrl, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        console.log("data", data.points);
+        let title = [];
+        data.points.map((each) => {
+          title.push(each.shift());
+        });
+        console.log("title: ", title);
+        setTitles(title);
+        setCoordsAr(data.points);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getData();
+    // fetch("http://localhost:3001/")
+    // .then((response) => response.json())
+    // .then((data) => setCoords(data));
+  }, []);
+
+  return (
+    <div id="whole">
+      <Header />
+      <br />
+      <br />
+      <button onClick={getData}>refresh</button> <br />
+      <Graph dataAr={coordsAr} titles={titles} />
+    </div>
+  );
+};
 export default Dashboard;
